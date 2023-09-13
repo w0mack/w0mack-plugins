36093bcfe1d8a3bf45a8a3cdb8fc1b14605ddae2
w0mack <harrison.womack@yahoo.com>
2023-09-12 23:34:46 -0500

    Initial Commit

---
+++ w0mack-mining/build.gradle.kts
@@ -0,0 +1,20 @@
+version = "0.0.1"
+
+project.extra["PluginName"] = "womack Mining"
+project.extra["PluginDescription"] = "Mines rocks"
+
+tasks {
+    jar {
+        manifest {
+            attributes(
+                mapOf(
+                    "Plugin-Version" to project.version,
+                    "Plugin-Id" to nameToId(project.extra["PluginName"] as String),
+                    "Plugin-Provider" to project.extra["PluginProvider"],
+                    "Plugin-Description" to project.extra["PluginDescription"],
+                    "Plugin-License" to project.extra["PluginLicense"]
+                )
+            )
+        }
+    }
+}
